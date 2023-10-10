876218a32606cdc4faf2bc20e3d8875f0f6bde5d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-10-10 20:06:03 +0900

    Execute rush

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -40,7 +40,7 @@ def prepare_triangle_sprite(args)
 end
 
 def prepare_circle_sprite(args, name, radius: 5)
-  diameter = radius * 2 + 1
+  diameter = (radius * 2) + 1
   radius_squared = radius**2
   render_target = args.outputs[name]
   render_target.width = diameter
@@ -75,7 +75,8 @@ end
 def update(args)
   player_inputs = args.state.player_inputs
   player = args.state.player
-  case player[:state][:type]
+  player_state = player[:state]
+  case player_state[:type]
   when :normal
     player[:v_x] = 0
     player[:v_y] = 0
@@ -101,8 +102,8 @@ def update(args)
       args.state.charge_particles = []
     end
   when :charging
-    player[:state][:ticks] += 1
-    player[:state][:power] = [player[:state][:ticks], 60].min
+    player_state[:ticks] += 1
+    player_state[:power] = [player_state[:ticks], 120].min
 
     args.state.charge_particles << player_charge_particle(player, args.state.sprites.circle)
 
@@ -113,12 +114,36 @@ def update(args)
     end
     args.state.charge_particles.reject! { |particle| Animations.finished?(particle[:animation]) }
 
-    player[:state] = { type: :normal } unless player_inputs[:charge]
+    unless player_inputs[:charge]
+      if player_state[:power] > 40
+        player[:state] = { type: :rushing, power: player_state[:power] }
+      else
+        player[:state] = { type: :normal }
+      end
+    end
+  when :rushing
+    execute_rush(player)
+    player[:state] = { type: :normal } if player_state[:power].zero?
   end
 end
 
+def execute_rush(player)
+  rushing_state = player[:state]
+  rushing_state[:power] = [rushing_state[:power] - 10, 0].max
+  return if rushing_state[:power].zero?
+
+  rush_speed = rushing_state[:power].idiv(2)
+  player[:x] += Math.cos(player[:face_angle]) * rush_speed
+  player[:y] += Math.sin(player[:face_angle]) * rush_speed
+end
+
 def render(args)
-  screen_render_target = Screen.build_render_target(args, args.state.screen)
+  screen = args.state.screen
+  screen_render_target = Screen.build_render_target(args, screen)
+  screen_render_target.sprites << {
+    x: 0, y: 0, w: screen[:x_resolution], h: screen[:y_resolution],
+    path: :pixel, **Colors::BACKGROUND
+  }
   player = args.state.player
   screen_render_target.sprites << [
     player_sprite(player),
@@ -130,8 +155,8 @@ def render(args)
     screen_render_target.sprites << args.state.charge_particles
   end
 
-  args.outputs.sprites << Screen.sprite(args.state.screen)
-  args.outputs.labels << { x: 0, y: 720, text: args.gtk.current_framerate.to_i.to_s }
+  args.outputs.sprites << Screen.sprite(screen)
+  args.outputs.labels << { x: 0, y: 720, text: args.gtk.current_framerate.to_i.to_s, **Colors::TEXT }
 end
 
 def player_sprite(player)
@@ -141,7 +166,7 @@ def player_sprite(player)
     w: player[:w],
     h: player[:h],
     path: :pixel,
-    r: 255, g: 0, b: 0
+    **Colors::PLAYER
   }
 end
 
@@ -149,9 +174,9 @@ def facing_triangle(entity, triangle_sprite, distance: 10)
   triangle_sprite.to_sprite(
     x: entity[:x] + Math.cos(entity[:face_angle]) * distance - triangle_sprite[:w].idiv(2),
     y: entity[:y] + Math.sin(entity[:face_angle]) * distance - triangle_sprite[:h].idiv(2),
-    r: 0, g: 0, b: 0,
     angle: entity[:face_angle].to_degrees,
-    angle_anchor_x: 0.5, angle_anchor_y: 0.5
+    angle_anchor_x: 0.5, angle_anchor_y: 0.5,
+    **Colors::DIRECTION_TRIANGLE
   )
 end
 
@@ -168,6 +193,48 @@ def player_charge_particle(player, circle_sprite)
     duration: 20
   )
   sprite
+end
+
+module DawnBringer32
+  BLACK = { r: 0x00, g: 0x00, b: 0x00 }.freeze
+  ALMOST_BLACK = { r: 0x22, g: 0x20, b: 0x34 }.freeze
+  DARK_BROWN = { r: 0x45, g: 0x28, b: 0x3c }.freeze
+  BROWN = { r: 0x66, g: 0x39, b: 0x31 }.freeze
+  LIGHT_BROWN = { r: 0x8f, g: 0x56, b: 0x3b }.freeze
+  ORANGE = { r: 0xdf, g: 0x71, b: 0x26 }.freeze
+  TAN = { r: 0xd9, g: 0xa0, b: 0x66 }.freeze
+  SKIN = { r: 0xee, g: 0xc3, b: 0x9a }.freeze
+  YELLOW = { r: 0xfb, g: 0xf2, b: 0x36 }.freeze
+  KIWI = { r: 0x99, g: 0xe5, b: 0x50 }.freeze
+  GREEN = { r: 0x6a, g: 0xbe, b: 0x30 }.freeze
+  CYAN = { r: 0x37, g: 0x94, b: 0x6e }.freeze
+  DARK_OLIVE_GREEN = { r: 0x4b, g: 0x69, b: 0x2f }.freeze
+  ARMY_GREEN = { r: 0x52, g: 0x4b, b: 0x24 }.freeze
+  ONYX = { r: 0x32, g: 0x3c, b: 0x39 }.freeze
+  AMERICAN_BLUE = { r: 0x3f, g: 0x3f, b: 0x74 }.freeze
+  METALLIC_BLUE = { r: 0x30, g: 0x60, b: 0x82 }.freeze
+  ROYAL_BLUE = { r: 0x5b, g: 0x6e, b: 0xe1 }.freeze
+  CORNFLOWER_BLUE = { r: 0x63, g: 0x9b, b: 0xff }.freeze
+  SKY_BLUE = { r: 0x5f, g: 0xcd, b: 0xe4 }.freeze
+  LAVENDER_BLUE = { r: 0xcb, g: 0xdb, b: 0xfc }.freeze
+  WHITE = { r: 0xff, g: 0xff, b: 0xff }.freeze
+  CADET_GREY = { r: 0x9b, g: 0xad, b: 0xb7 }.freeze
+  OLD_SILVER = { r: 0x84, g: 0x7e, b: 0x87 }.freeze
+  DIM_GRAY = { r: 0x69, g: 0x6a, b: 0x6a }.freeze
+  GRAY = { r: 0x52, g: 0x55, b: 0x59 }.freeze
+  VIOLET = { r: 0x76, g: 0x42, b: 0x8a }.freeze
+  RED = { r: 0xac, g: 0x32, b: 0x32 }.freeze
+  LIGHT_RED = { r: 0xd9, g: 0x57, b: 0x63 }.freeze
+  PINK = { r: 0xd7, g: 0x7b, b: 0xba }.freeze
+  MOSS_GREEN = { r: 0x8f, g: 0x97, b: 0x41 }.freeze
+  ANOTHER_BROWN = { r: 0x8a, g: 0x6f, b: 0x30 }.freeze
+end
+
+module Colors
+  BACKGROUND = DawnBringer32::GRAY
+  TEXT = DawnBringer32::WHITE
+  DIRECTION_TRIANGLE = DawnBringer32::WHITE
+  PLAYER = DawnBringer32::LAVENDER_BLUE
 end
 
 $gtk.reset
