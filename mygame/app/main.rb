c5aefb18ac76da5032bb60f8ba61bba91d8a267d
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-10-10 15:39:59 +0900

    Faacing triangle

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -12,7 +12,27 @@ end
 
 def setup(args)
   args.state.screen = Screen::GBA_STYLE
-  args.state.player = { x: 160, y: 90 }
+  args.state.player = {
+    x: 160, y: 90, w: 9, h: 9,
+    face_angle: 0, v_x: 0, v_y: 0
+  }
+  prepare_sprites(args)
+end
+
+def prepare_sprites(args)
+  args.state.sprites.triangle = prepare_triangle_sprite(args)
+end
+
+def prepare_triangle_sprite(args)
+  render_target = args.outputs[:triangle]
+  render_target.width = 5
+  render_target.height = 5
+  render_target.sprites << [
+    { x: 0, y: 0, w: 1, h: 5, path: :pixel },
+    { x: 1, y: 1, w: 1, h: 3, path: :pixel },
+    { x: 2, y: 2, w: 1, h: 1, path: :pixel }
+  ]
+  { w: 5, h: 5, path: :triangle }
 end
 
 def process_input(args)
@@ -42,19 +62,43 @@ def update(args)
   elsif player_inputs[:down]
     player[:v_y] = -1
   end
+  return if player[:v_x].zero? && player[:v_y].zero?
 
   player[:x] += player[:v_x]
   player[:y] += player[:v_y]
+  player[:face_angle] = Math.atan2(player[:v_y], player[:v_x])
 end
 
 def render(args)
   screen_render_target = Screen.build_render_target(args, args.state.screen)
   player = args.state.player
-  screen_render_target.sprites << {
-    x: player[:x] - 5, y: player[:y] - 5, w: 10, h: 10, path: :pixel,
+  screen_render_target.sprites << [
+    player_sprite(player),
+    facing_triangle(player, args.state.sprites.triangle)
+  ]
+
+  args.outputs.sprites << Screen.sprite(args.state.screen)
+end
+
+def player_sprite(player)
+  {
+    x: player[:x] - player[:w].idiv(2),
+    y: player[:y] - player[:h].idiv(2),
+    w: player[:w],
+    h: player[:h],
+    path: :pixel,
     r: 255, g: 0, b: 0
   }
-  args.outputs.sprites << Screen.sprite(args.state.screen)
+end
+
+def facing_triangle(entity, triangle_sprite, distance: 10)
+  triangle_sprite.to_sprite(
+    x: entity[:x] + Math.cos(entity[:face_angle]) * distance - triangle_sprite[:w].idiv(2),
+    y: entity[:y] + Math.sin(entity[:face_angle]) * distance - triangle_sprite[:h].idiv(2),
+    r: 0, g: 0, b: 0,
+    angle: entity[:face_angle].to_degrees,
+    angle_anchor_x: 0.5, angle_anchor_y: 0.5
+  )
 end
 
 $gtk.reset
