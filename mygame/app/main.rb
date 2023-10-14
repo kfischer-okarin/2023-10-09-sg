8b7b18b0badb292d3348b04df3bfa1e7162d03b1
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-10-14 12:46:34 +0900

    Move Player related logic to own module

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,5 +1,6 @@
 require 'app/colors.rb'
 require 'app/collision.rb'
+require 'app/player.rb'
 require 'lib/animations.rb'
 require 'lib/screen.rb'
 
@@ -16,12 +17,7 @@ end
 def setup(args)
   args.state.screen = Screen::GBA_STYLE
   args.state.game_state = :playing
-  args.state.player = {
-    x: 1600, y: 900, w: 9, h: 9,
-    face_angle: 0, v_x: 0, v_y: 0,
-    state: { type: :movement },
-    collision_radius: 50
-  }
+  args.state.player = Player.build(x: 1600, y: 900)
   args.state.crescent_moon = {
     x: 2000, y: 1000, w: 11, h: 11,
     face_angle: 0, v_x: 0, v_y: 0,
@@ -89,10 +85,7 @@ def process_input(args)
 end
 
 def update(args)
-  player = args.state.player
-  player_state = player[:state]
-
-  send("handle_player_#{player_state[:type]}", args, player)
+  Player.tick(args, args.state.player)
   return if args.state.game_state == :won
 
   scale = WORLD_TO_SCREEN_SCALE
@@ -106,41 +99,6 @@ def update(args)
   end
 end
 
-def handle_player_movement(args, player)
-  return if args.state.game_state == :won
-
-  player_inputs = args.state.player_inputs
-
-  update_player_velocity(player, player_inputs)
-  update_face_angle(player, player[:v_x], player[:v_y]) if moving?(player)
-  return unless player_inputs[:charge]
-
-  start_charging(args, player)
-end
-
-PLAYER_SPEED = 15
-PLAYER_DIAGONAL_SPEED = (PLAYER_SPEED / Math.sqrt(2)).round
-
-def update_player_velocity(player, player_inputs)
-  player[:v_x] = 0
-  player[:v_y] = 0
-
-  if player_inputs[:left]
-    player[:v_x] = -1
-  elsif player_inputs[:right]
-    player[:v_x] = 1
-  end
-
-  if player_inputs[:up]
-    player[:v_y] = 1
-  elsif player_inputs[:down]
-    player[:v_y] = -1
-  end
-  speed = moving_diagonally?(player) ? PLAYER_DIAGONAL_SPEED : PLAYER_SPEED
-  player[:v_x] *= speed
-  player[:v_y] *= speed
-end
-
 def update_face_angle(entity, direction_x, direction_y)
   entity[:face_angle] = Math.atan2(direction_y, direction_x)
 end
@@ -151,80 +109,6 @@ end
 
 def moving_diagonally?(entity)
   entity[:v_x].nonzero? && entity[:v_y].nonzero?
-end
-
-def start_charging(args, player)
-  player[:state] = { type: :charging, ticks: 0, power: 0 }
-  player[:v_x] = 0
-  player[:v_y] = 0
-
-  args.state.charge_particles = []
-end
-
-def handle_player_charging(args, player)
-  player_inputs = args.state.player_inputs
-  charging_state = player[:state]
-
-  if player_inputs[:charge]
-    charging_state[:ticks] += 1
-    charging_state[:power] = [charging_state[:ticks], 120].min
-    charging_state[:ready] = charging_state[:power] >= 40
-
-    particles = args.state.charge_particles
-    particles << player_charge_particle(player, args.state.sprites.circle)
-
-    particles.each do |particle|
-      Animations.perform_tick(particle[:animation])
-      particle[:x] = scaled_to_screen(player[:x] + Math.cos(particle[:angle_from_player]) * particle[:distance])
-      particle[:y] = scaled_to_screen(player[:y] + Math.sin(particle[:angle_from_player]) * particle[:distance])
-    end
-    particles.reject! { |particle| Animations.finished?(particle[:animation]) }
-
-    charging_state[:predicted_distance] = predict_rush_distance(player) if charging_state[:ready]
-  else
-    if charging_state[:ready]
-      player[:state] = { type: :rushing, power: charging_state[:power] }
-    else
-      player[:state] = { type: :movement }
-    end
-  end
-end
-
-def predict_rush_distance(player)
-  simulated_player = player.dup
-  simulated_player[:state] = { type: :rushing, power: player[:state][:power] }
-  distance_x = 0
-  distance_y = 0
-  until simulated_player[:state][:power].zero?
-    execute_rush(simulated_player)
-    distance_x += simulated_player[:v_x]
-    distance_y += simulated_player[:v_y]
-  end
-  Math.sqrt((distance_x**2) + (distance_y**2))
-end
-
-def handle_player_rushing(args, player)
-  rushing_state = player[:state]
-  execute_rush(player)
-
-  enemies = args.state.enemies
-  hit_enemies = moving_entity_collisions(player, enemies)
-  hit_enemies.each do |enemy|
-    enemy[:state] = { type: :dead, ticks: 0 }
-  end
-  args.state.game_state = :won if enemies.all? { |enemy| enemy[:state][:type] == :dead }
-
-  player[:state] = { type: :movement } if rushing_state[:power].zero?
-end
-
-def execute_rush(player)
-  rushing_state = player[:state]
-  rushing_state[:power] = [rushing_state[:power] - 10, 0].max
-  return if rushing_state[:power].zero?
-
-  rush_speed = rushing_state[:power].idiv(2) * 10
-  player[:v_x] = Math.cos(player[:face_angle]) * rush_speed
-  player[:v_y] = Math.sin(player[:face_angle]) * rush_speed
 end
 
 def moving_entity_collisions(entity, targets)
@@ -247,7 +131,7 @@ def render(args)
   screen_render_target.sprites << crescent_moon_sprite(args.state.crescent_moon)
 
   player = args.state.player
-  screen_render_target.sprites << player_sprite(player)
+  screen_render_target.sprites << Player.sprite(player)
   player_facing_triangle = facing_triangle(player, args.state.sprites.triangle)
 
   player_state = player[:state]
@@ -276,17 +160,6 @@ def render(args)
 
   args.outputs.sprites << Screen.sprite(screen)
   args.outputs.labels << { x: 0, y: 720, text: args.gtk.current_framerate.to_i.to_s, **Colors::TEXT }
-end
-
-def player_sprite(player)
-  {
-    x: scaled_to_screen(player[:x]) - player[:w].idiv(2),
-    y: scaled_to_screen(player[:y]) - player[:h].idiv(2),
-    w: player[:w],
-    h: player[:h],
-    path: :pixel,
-    **Colors::PLAYER
-  }
 end
 
 def crescent_moon_sprite(crescent_moon)
---
+++ mygame/app/player.rb
@@ -0,0 +1,141 @@
+module Player
+  class << self
+    def build(x:, y:)
+      {
+        type: Player,
+        x: x, y: y, w: 9, h: 9,
+        face_angle: 0, v_x: 0, v_y: 0,
+        state: { type: :movement },
+        collision_radius: 50
+      }
+    end
+
+    def tick(args, player)
+      player_state = player[:state]
+
+      send("handle_#{player_state[:type]}", args, player)  
+    end
+
+    def sprite(player)
+      {
+        x: scaled_to_screen(player[:x]) - player[:w].idiv(2),
+        y: scaled_to_screen(player[:y]) - player[:h].idiv(2),
+        w: player[:w],
+        h: player[:h],
+        path: :pixel,
+        **Colors::PLAYER
+      }
+    end
+
+    def handle_movement(args, player)
+      return if args.state.game_state == :won
+
+      player_inputs = args.state.player_inputs
+
+      update_velocity(player, player_inputs)
+      update_face_angle(player, player[:v_x], player[:v_y]) if moving?(player)
+      return unless player_inputs[:charge]
+
+      start_charging(args, player)
+    end
+
+    PLAYER_SPEED = 15
+    PLAYER_DIAGONAL_SPEED = (PLAYER_SPEED / Math.sqrt(2)).round
+
+    def update_velocity(player, player_inputs)
+      player[:v_x] = 0
+      player[:v_y] = 0
+
+      if player_inputs[:left]
+        player[:v_x] = -1
+      elsif player_inputs[:right]
+        player[:v_x] = 1
+      end
+
+      if player_inputs[:up]
+        player[:v_y] = 1
+      elsif player_inputs[:down]
+        player[:v_y] = -1
+      end
+      speed = moving_diagonally?(player) ? PLAYER_DIAGONAL_SPEED : PLAYER_SPEED
+      player[:v_x] *= speed
+      player[:v_y] *= speed
+    end
+
+    def start_charging(args, player)
+      player[:state] = { type: :charging, ticks: 0, power: 0 }
+      player[:v_x] = 0
+      player[:v_y] = 0
+
+      args.state.charge_particles = []
+    end
+
+    def handle_charging(args, player)
+      player_inputs = args.state.player_inputs
+      charging_state = player[:state]
+
+      if player_inputs[:charge]
+        charging_state[:ticks] += 1
+        charging_state[:power] = [charging_state[:ticks], 120].min
+        charging_state[:ready] = charging_state[:power] >= 40
+
+        particles = args.state.charge_particles
+        particles << player_charge_particle(player, args.state.sprites.circle)
+
+        particles.each do |particle|
+          Animations.perform_tick(particle[:animation])
+          particle[:x] = scaled_to_screen(player[:x] + Math.cos(particle[:angle_from_player]) * particle[:distance])
+          particle[:y] = scaled_to_screen(player[:y] + Math.sin(particle[:angle_from_player]) * particle[:distance])
+        end
+        particles.reject! { |particle| Animations.finished?(particle[:animation]) }
+
+        charging_state[:predicted_distance] = predict_rush_distance(player) if charging_state[:ready]
+      else
+        if charging_state[:ready]
+          player[:state] = { type: :rushing, power: charging_state[:power] }
+        else
+          player[:state] = { type: :movement }
+        end
+      end
+    end
+
+    def predict_rush_distance(player)
+      simulated_player = player.dup
+      simulated_player[:state] = { type: :rushing, power: player[:state][:power] }
+      distance_x = 0
+      distance_y = 0
+      until simulated_player[:state][:power].zero?
+        execute_rush(simulated_player)
+        distance_x += simulated_player[:v_x]
+        distance_y += simulated_player[:v_y]
+      end
+      Math.sqrt((distance_x**2) + (distance_y**2))
+    end
+
+    def handle_rushing(args, player)
+      rushing_state = player[:state]
+      execute_rush(player)
+
+      enemies = args.state.enemies
+      hit_enemies = moving_entity_collisions(player, enemies)
+      hit_enemies.each do |enemy|
+        enemy[:state] = { type: :dead, ticks: 0 }
+      end
+      args.state.game_state = :won if enemies.all? { |enemy| enemy[:state][:type] == :dead }
+
+      player[:state] = { type: :movement } if rushing_state[:power].zero?
+    end
+
+    def execute_rush(player)
+      rushing_state = player[:state]
+      rushing_state[:power] = [rushing_state[:power] - 10, 0].max
+      return if rushing_state[:power].zero?
+
+      rush_speed = rushing_state[:power].idiv(2) * 10
+      player[:v_x] = Math.cos(player[:face_angle]) * rush_speed
+      player[:v_y] = Math.sin(player[:face_angle]) * rush_speed
+    end
+
+    
+  end
+end
